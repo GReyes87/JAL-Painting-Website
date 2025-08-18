fcbeb15bef49fc1dd59b42bf9359c7b36de62c95
Jefferson Morales <jeffersonmorales1022@gmail.com>
2025-08-18 17:11:56 -0400

    css style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Services from "./pages/services";
 import Contact from "./pages/contact"; 
+import './global.css'; // Import global styles
 
 
 function App() {
